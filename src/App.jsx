abc2fe762fca71425ffa63bc26d28f58ac68ede3
Dhanushkpt <dhanushrao435@gmail.com>
2024-10-04 14:47:06 +0530

    Done !!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./components/Home";
+import Home from "./components/Home"; 
 import About from "./components/About";
 import PortFolio from "./Components/PortFolio";
 import Experiance from "./components/Experiance";
